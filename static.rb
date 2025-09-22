a4d4220db6c10600ca289c57034f291e7f0db1ed
adithyasource <adithyasrc@gmail.com>
2025-09-22 12:40:47 +0530

    feat: updated static final

--- static.rb
+++ static.rb
@@ -2,8 +2,8 @@ class Static < Formula
   desc "a local copy of your spotify, statically stored"
   homepage "https://github.com/adithyasource/static"
   url "https://github.com/adithyasource/static/releases/download/1.0/static-1.0.tar.gz"
-  sha256 "7871d729af276cb5d4791f08d820f90d4af2b20a18ad4060c440852f1474473a"
-  version "0.1.0"
+  sha256 "2108b84c30a9231315919e9c9d50d64446d3fb73533d291ef0ad09953aeb539b"
+  version "1.0"
 
   def install
     bin.install "static"
