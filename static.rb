d8d175610ea1cdb158010bcbd04c5b7c10cc7a00
adithyasource <adithyasrc@gmail.com>
2026-01-08 19:58:36 +0530

    chore: updated static to 1.1.2

--- static.rb
+++ static.rb
@@ -1,9 +1,9 @@
 class Static < Formula
   desc "a local copy of your spotify, statically stored"
   homepage "https://github.com/adithyasource/static"
-  url "https://github.com/adithyasource/static/releases/download/1.1.1/static-1.1.1.tar.gz"
-  sha256 "660d5d2a255180e4bd7a5e8528596149ec458e56a5995c60deb6699148989c47"
-  version "1.1.1"
+  url "https://github.com/adithyasource/static/releases/download/1.1.2/static-1.1.2.tar.gz"
+  sha256 "717c49bc6f67cfd5f351586b2174696a92f23ddb1f59c68b8b7e0d42b2d45fc8"
+  version "1.1.2"
 
   def install
     bin.install "static"
