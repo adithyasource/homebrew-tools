09b2c0d372fb1f7224e04f4187c988e8d0e62568
adithyasource <adithyasrc@gmail.com>
2026-05-13 16:12:53 +0530

    chore: bump to 1.1.3

--- static.rb
+++ static.rb
@@ -1,9 +1,9 @@
 class Static < Formula
-  desc "a local copy of your spotify, statically stored"
+  desc "a local, static copy of your spotify"
   homepage "https://github.com/adithyasource/static"
-  url "https://github.com/adithyasource/static/releases/download/1.1.2/static-1.1.2.tar.gz"
-  sha256 "717c49bc6f67cfd5f351586b2174696a92f23ddb1f59c68b8b7e0d42b2d45fc8"
-  version "1.1.2"
+  url "https://github.com/adithyasource/static/releases/download/1.1.3/static-1.1.3.tar.gz"
+  sha256 "dc855cb63cd8fb2945c7ea03b79b078ddaabcb5b0406cc71a304f822d5c0b448"
+  version "1.1.3"
 
   def install
     bin.install "static"
